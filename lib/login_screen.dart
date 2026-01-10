927f9b4cb973bd2e19376793babdc84712f56f80
crowTweak <awspickup448@gmail.com>
2026-01-10 16:11:06 +0300

    Update login_screen.dart

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dashboard_screen.dart'; // استيراد الرئيسية
+import 'dashboard_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
