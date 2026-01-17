091de69f84144a6d7dedd17917a5f3e697c83690
crowTweak <awspickup448@gmail.com>
2026-01-17 16:58:02 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart'; // إضافة مكتبة فيربيز الأساسية
+import 'firebase_options.dart'; // إضافة ملف الإعدادات الذي أنشأته
 import 'onboarding_screen.dart'; 
 import 'signup_screen.dart';     
 
-void main() {
+void main() async {
+  // التأكد من أن جميع أدوات فلاتر جاهزة قبل بدء التهيئة
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // تهيئة تطبيق فيربيز باستخدام الإعدادات اليدوية التي وضعتها
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const RaseedApp());
 }
 
